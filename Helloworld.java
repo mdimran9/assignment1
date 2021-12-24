14b3df2f7cc3da39d012af98aa1543cf4eab8f09
mdimran9 <96309234+mdimran9@users.noreply.github.com>
2021-12-25 01:20:16 +0530

    Add files via upload

---
+++ Helloworld.java
@@ -0,0 +1,13 @@
+package project3;
+
+public class Helloworld {
+
+		public static void main(String[] args) {
+			
+			System.out.println("Hello World");
+			System.out.println("Welcome to Geekster");
+			System.out.print("My name is 'md imran'");
+		}
+
+	}
+
